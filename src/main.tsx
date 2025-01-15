2547c42c817063b90047ab65b54afc125710cda1
Willian Igor <willanigordeveloper@gmail.com>
2025-01-15 16:55:57 -0300

    add react-router-doom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ROUTES } from "@utils"
 import "./index.css"
 // Contexts
 import { GlobalProvider } from "@contexts/_global"
+// Pages
+import { IndexPage } from "@pages/_index"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalProvider>
-      <div />
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES.INDEX} element={<IndexPage />} />
+        </Routes>
+      </BrowserRouter>
     </GlobalProvider>
   </StrictMode>,
 )
